428c6d9cda3043c5732026caa562d86939ee92a1
chuuniby <takanashirikka501@gmail.com>
2023-05-09 23:22:48 +0800

    Attempt to add freezing chip
    It can find the nearest enemy and make the enemy stop moving and turn it into a "platform". However, wall jump doesn't work on it yet.
    Need to revamp the movement system first before moving on

--- Assets/FreezingChipScript.cs
+++ Assets/FreezingChipScript.cs
@@ -6,21 +6,37 @@ using System.Linq;
 
 public class FreezingChipScript : MonoBehaviour
 {
-    public GameObject[] enemies;
-    public GameObject enemy;
-    public List<float> distances;
-    public float distance;
     public float minDistance;
+    public GameObject minEnemy;
+    public GameObject[] enemies;
 
-    void FindNearestEnemy()
+    private void Start()
+    {
+        minDistance = Mathf.Infinity;
+        minEnemy = null;
+    }
+
+    public void FindNearestEnemy()
     {
         enemies = GameObject.FindGameObjectsWithTag("Enemy");
         foreach (GameObject enemy in enemies)
         {
-            distance = Vector3.Distance(transform.position, enemy.transform.position);
-            distances.Add(distance);
-            minDistance = distances.AsQueryable().Min();
-            //int index = distances.FindIndex(minDistance);
+            float distance = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distance < minDistance)
+            {
+                minDistance = distance;
+                minEnemy = enemy;
+            }
+        }
+    }
+
+    public void FreezeEnemy()
+    {
+        FindNearestEnemy();
+        if (minEnemy != null)
+        {
+            minEnemy.transform.tag = "Platform";
+            minEnemy.transform.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
         }
     }
 }
